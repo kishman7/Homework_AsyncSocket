5ad1fdbd3b806eb52ac7d7868bf2c545753ec9ef
kishman7 <kishman_us@ukr.net>
2021-11-24 20:24:39 +0200

    Add Homework_AsyncSocket

---
+++ AsyncClient/AsyncClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AsyncClient
+{
+    class AsyncClient
+    {
+        private const int Port = 2020;
+        private const int Backlog = 100;
+        private const int Size = 100;
+
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Process.Start("test.exe");
+            //}
+            Console.Title = "Client";
+
+            StartClient();
+
+            Console.ReadLine();
+        }
+
+        private static void StartClient()
+        {
+            IPHostEntry entry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = entry.AddressList[0];
+            // Socket
+            Socket client = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Connect
+                client.BeginConnect(new IPEndPoint(ip, Port), ConnectCallBack, client);
+            }
+            catch (SocketException ex)
+            {
+            }
+        }
+
+        private static void ConnectCallBack(IAsyncResult ar)
+        {
+            // встановлюємо з’єднання
+            var client = (Socket)ar.AsyncState;
+            client.EndConnect(ar);
+            Console.WriteLine("Enter data to get a date or time! (Enter date or time)");
+
+            var data = Encoding.UTF8.GetBytes(Console.ReadLine());
+
+            // Send
+            client.BeginSend(data, 0, data.Length, SocketFlags.None, SendCallback, client);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            var client = (Socket)ar.AsyncState;
+            int countBytes = client.EndSend(ar);
+            Console.WriteLine("Send to server {0} bytes", countBytes);
+
+            ReceiveFrom(client);
+        }
+
+        private static void ReceiveFrom(Socket client)
+        {
+            var buffer = new byte[Size];
+            var data = new
+            {
+                Socket = client,
+                Buffer = buffer,
+                Size = Size
+            };
+            client.BeginReceive(buffer, 0, Size, SocketFlags.None, ReceiveCallback, data);
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            var data = (dynamic)ar.AsyncState;
+            var client = (Socket)data.Socket;
+            var buffer = (byte[])data.Buffer;
+
+            var countBytes = client.EndReceive(ar);
+
+            var responce = Encoding.UTF8.GetString(buffer, 0, countBytes);
+
+            Console.WriteLine("Received from server ({0}): {1}", client.RemoteEndPoint, responce);
+        }
+    }
+}
---
+++ AsyncServer/AsyncServer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace AsyncServer
+{
+    class AsyncServer
+    {
+        private const int Port = 2020;
+        private const int Backlog = 10;
+        private const int Size = 100;
+        static AutoResetEvent done = new AutoResetEvent(false); //подія для здійснення синхронізації потоків
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            StartServer();
+        }
+
+        private static void StartServer()
+        {
+            IPHostEntry entry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = entry.AddressList[0];
+            //1 new Socket
+            Socket server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //2 Bind(EP) -> EP = IP:port
+                server.Bind(new IPEndPoint(ip, Port)); //байндимо сервер з IP та портом
+                //3 Listen
+                server.Listen(Backlog); // сервер слухає одночасно кількість клієнтів
+
+                while (true)
+                {
+                    Console.WriteLine("Wait for connection...");
+                    //4 Accept
+                    server.BeginAccept(AcceptCallback, server); // через асинхронний делегат BeginAccept виділяємо окремий потік для підключення клієнта
+                    done.WaitOne(); // чекаю на сигнал. Якщо подію переведуть в сигнальний стан, то тоді цикл повторить іншу ітерацію, інакше потік буде за блокований
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            done.Set(); //встановлюємо подію в  сигнальний стан. Значить тут все ОК і ми готові слухати нового клієнта
+            var server = (Socket)ar.AsyncState; // звертаємось до сервера через параметер ar з делегата BeginAccept
+            //5) work with client (Receive)
+            var client = server.EndAccept(ar); // отримуємо клієнтський Socket
+            var buffer = new byte[Size];
+            var data = new //TransferObject -> struct, class, який потрібно реалізувати вище
+            {
+                Socket = client,
+                Buffer = buffer,
+                Size = Size
+            };
+            client.BeginReceive(buffer, 0, Size, SocketFlags.None, ReceiveCallBack, data);
+        }
+
+        private static void ReceiveCallBack(IAsyncResult ar)
+        {
+            var data = (dynamic)ar.AsyncState;
+            //var client = data.GetType().GetProperty("Socket").GetValue(data); // один з варіантів використання data (складнійший)
+            var client = (Socket)data.Socket; // інший з варіантів використання data (простійший)
+            var countBytes = client.EndReceive(ar); // повертаємо кількість байтів отриманих від клієнта
+            var buffer = (byte[])data.Buffer;
+            string result = Encoding.UTF8.GetString(buffer, 0, countBytes);
+            Console.WriteLine("Got: {0} from {1}", result, client.RemoteEndPoint);
+
+            //6) Send responce
+            SendTo(client, result); //повертаємо повідомлення для нашого клієнта
+        }
+
+        private static void SendTo(Socket client, string result)
+        {
+            Console.WriteLine("Receive: " + result); //виводимо повідомлення, яке отримаємо
+            var responce = Encoding.UTF8.GetBytes(result);
+
+            if (result.Equals("date", StringComparison.CurrentCultureIgnoreCase))
+            {
+                client.BeginSend(Encoding.UTF8.GetBytes(DateTime.Now.ToShortDateString()), 0, responce.Length, SocketFlags.None, SendCallBack, client);
+
+                //client.Send(Encoding.UTF8.GetBytes(DateTime.Now.ToShortDateString()));  //відправляємо дані клієнту
+            }
+            else if (result.Equals("time", StringComparison.CurrentCultureIgnoreCase))
+            {
+                client.Send(Encoding.UTF8.GetBytes(DateTime.Now.ToShortTimeString()));  //відправляємо дані клієнту
+            }
+            else
+            {
+                client.Send(Encoding.UTF8.GetBytes("Команда не існує!!!"));  //відправляємо дані клієнту
+            }
+            client.BeginSend(responce, 0, responce.Length, SocketFlags.None, SendCallBack, client);
+        }
+
+        private static void SendCallBack(IAsyncResult ar)
+        {
+            var client = (Socket)ar.AsyncState; // звертаємось до клієнта через параметер ar з делегата BeginSend
+            int countBytes = client.EndSend(ar); // отримуємо кількість байт від клієнта
+
+            Console.WriteLine("OK. Sent {0} bytes to client", countBytes);
+
+            // 7) Close
+            client.Close();
+        }
+    }
+}
